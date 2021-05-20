50231aa7ba6be5b40ca98a01a4b745fc0af3b860
Josué Guardado <jguardadosv@outlook.com>
2021-05-20 09:20:29 -0600

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void Trabajo()
+        {
+            Thread.Sleep(100);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                //CHECK FOR CANCELLATION FIRST
+                if (backgroundWorker1.CancellationPending)
+                {
+                    //CANCEL
+                    e.Cancel = true;
+                }
+                else
+                {
+                    Trabajo();
+                    backgroundWorker1.ReportProgress(i);
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            label1.Text = e.ProgressPercentage.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+    }
+}
